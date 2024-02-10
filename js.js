97db3f7003e8a6acda052518c26148ef1fbd5b7c
Nicolas Lema <141177866+nicolema02@users.noreply.github.com>
2024-02-10 17:28:02 -0300

    Update js.js
    Ajustes

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function cambiarMensaje() {
     var mensajes = [
         "Sos hermosa mi pucherito",
         "Tu sonrisa tiene el poder de iluminar hasta el dia mas oscuro de mi vida",
-        "Sos la mujer mas hermosa en el universo",
+        "Sos la mujer más hermosa en el universo",
         "Contigo soy la persona más feliz del mundo",
         "Quiero pasar el resto de mi vida junto a vos preciosa",
         "Sos el amor de mi vida",
@@ -11,7 +11,8 @@ function cambiarMensaje() {
         "Tu carita es lo mas hermoso del mundo <3",
         "Ojala me dejes compartir el resto de mi vida contigo",
         "Ojala poder hacerte feliz como te mereces mi amor, mereces todo en este mundo",
-        "Sos la persona con el alma mas pura que conoci en mi vida, no tenes ni un gramo de maldad y eso me enamoro <3"
+        "Sos la persona con el alma más pura que conocí en mi vida, no tenes ni un gramo de maldad y eso me enamoro <3"
+
 
     ];
     var indice = Math.floor(Math.random() * mensajes.length);
